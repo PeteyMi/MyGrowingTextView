908be8ccbfaa897f108c67134cbd9605ac107ad4
PeteyMi <emmet2009@163.com>
2014-05-29 00:23:10 +0800

    Add new project file

---
+++ MyGrowingTextViewDemo/MyGrowingTextView/MyGrowingTextView.h
@@ -0,0 +1,13 @@
+//
+//  MyGrowingTextView.h
+//  MyGrowingTextViewDemo
+//
+//  Created by Petey on 5/29/14.
+//  Copyright (c) 2014 Petey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyGrowingTextView : UIView
+
+@end
---
+++ MyGrowingTextViewDemo/MyGrowingTextViewDemo/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  MyGrowingTextViewDemo
+//
+//  Created by Petey on 5/29/14.
+//  Copyright (c) 2014 Petey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
